94927fe49190b2bc03bdd7c6dd2f172ff6b3afa5
Rayne58 <mfalicea@buffalo.edu>
2016-03-18 17:09:07 -0400

    First commit, tutorial all done, starting on Android work!

---
+++ Kurotama-The-Return/Assets/Scripts/ControllerPhone.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllerPhone : MonoBehaviour {
+	public int screenWidth = Screen.width;
+	public int screenHeight = Screen.height;
+
+
+	void Start() {
+	}
+		
+}
+
+
