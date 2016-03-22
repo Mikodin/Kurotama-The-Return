ebcef97b451a49bb30fef47f3f3653aecdc47c14
Rayne58 <mfalicea@buffalo.edu>
2016-03-22 15:02:39 -0400

    Maybe it's okay now!

---
+++ Assets/Scripts/MobileInput.cs
@@ -0,0 +1,190 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MobileInput : MonoBehaviour
+{
+	int screenWidth = Screen.width;
+	int screenHeight = Screen.height;
+	
+	public float dividerPos;
+	Pane leftPane;
+	Pane rightPane;
+	public float noActionRadius;
+
+	Touch touches;
+
+	int leftAction = 0;
+	int rightAction = 0;
+	Gesture g1;
+
+	void Start ()
+	{
+		dividerPos = screenWidth / 3;
+		noActionRadius = dividerPos / 5;
+		leftPane = new Pane (0, dividerPos);
+		rightPane = new Pane (dividerPos, screenWidth);
+		g1 = new Gesture (1);
+	}
+
+	void Update ()
+	{
+		foreach (Touch touch in Input.touches) {
+			int x = 1;
+			switch (touch.phase) {
+
+			case TouchPhase.Began:
+				g1.SetStart (touch.position);
+				g1.SetGesture ("taphold");
+				break;
+
+			case TouchPhase.Moved:
+				g1.SetEnd (touch.position);
+				g1.CalculateDirection ();
+				print ("Gesture Direction: " + g1.GetDirection ());
+				g1.CalculateGesture ();
+				break;
+
+			case TouchPhase.Ended:
+				print (g1.GetGesture ());
+				g1.Reset ();
+				break;
+			}
+		}
+	}
+
+	void SetLeftAction (int _action)
+	{
+		leftAction = _action;
+	}
+
+	public int GetLeftAction ()
+	{
+		return leftAction;
+	}
+
+	void SetRightAction (int _action)
+	{
+		rightAction = _action;
+	}
+
+	public int GetRightAction ()
+	{
+		return rightAction;
+	}
+
+	public float GetNoActionRadius() {
+		return noActionRadius;
+	}
+
+	public string GetGestures() {
+		return g1.GetGesture();
+	}
+
+	struct Pane
+	{
+		public float startPos;
+		public float endPos;
+
+		public Pane (float _startPos, float _endPos)
+		{
+			startPos = _startPos;
+			endPos = _endPos;
+		}
+
+		public bool InBounds (Touch _touch)
+		{
+			if ((_touch.position.x > startPos && _touch.position.x < endPos) && (_touch.position.y > 0 && _touch.position.y < Screen.height))
+				return true;
+			else
+				return false;
+		}
+
+	}
+
+	struct Gesture
+	{
+		Vector2 start;
+		Vector2 end;
+		Vector2 direction; 
+		string gesture;
+		float noActionRadius;
+
+		public Gesture (int i)
+		{
+			start = new Vector2(0,0);
+			end = new Vector2 (0, 0);
+			direction = new Vector2 (0, 0);
+			gesture = "";
+			noActionRadius = (Screen.width/3)/5;
+		}
+
+		public void SetStart (Vector2 _start)
+		{
+			start = _start;
+		}
+
+		public Vector2 GetStart ()
+		{
+			return start;
+		}
+
+		public void SetEnd (Vector2 _end)
+		{
+			end = _end;
+		}
+
+		public Vector2 GetEnd ()
+		{
+			return end;
+		}
+
+		public Vector2 GetDirection ()
+		{
+			return direction;
+		}
+
+		public void CalculateDirection ()
+		{
+			direction = end - start;
+		} 
+
+		public void CalculateGesture() {
+			if (Mathf.Abs (direction.x) < noActionRadius && Mathf.Abs (direction.y) < noActionRadius)
+				SetGesture ("taphold");
+			else if (direction.x > noActionRadius && direction.y > noActionRadius)
+				SetGesture ("upright");
+			else if (direction.x < -noActionRadius && direction.y > noActionRadius)
+				SetGesture ("upleft");
+			else if (direction.x < -noActionRadius && direction.y < -noActionRadius)
+				SetGesture ("downleft");
+			else if (direction.x > noActionRadius && direction.y < -noActionRadius)
+				SetGesture ("downright");
+			else if ((direction.x > -noActionRadius || direction.x < noActionRadius) && direction.y > noActionRadius)
+				SetGesture ("up");
+			else if ((direction.x > -noActionRadius || direction.x < noActionRadius) && direction.y < -noActionRadius)
+				SetGesture ("down");
+			else if ((direction.y > -noActionRadius || direction.y < noActionRadius) && direction.x > noActionRadius)
+				SetGesture ("right");
+			else if ((direction.y > -noActionRadius || direction.y < noActionRadius) && direction.x < -noActionRadius)
+				SetGesture ("left");
+		}
+
+		public void SetGesture(string _gesture) {
+			gesture = _gesture;
+		}
+
+		public string GetGesture() {
+			return gesture;
+		}
+
+		public void Reset() {
+			start = new Vector2 (0, 0);
+			end = new Vector2 (0, 0);
+			direction = new Vector2 (0, 0);
+			SetGesture ("");
+		}
+	}
+
+}
+
+
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Controller2D))]
+[RequireComponent (typeof (MobileInput))]
+public class Player : MonoBehaviour {
+
+	public float maxJumpHeight = 4;
+	public float minJumpHeight = 1;
+	public float timeToJumpApex = .4f;
+	float accelerationTimeAirborne = .2f;
+	float accelerationTimeGrounded = .1f;
+	float moveSpeed = 6;
+
+	public Vector2 wallJumpClimb;
+	public Vector2 wallJumpOff;
+	public Vector2 wallLeap;
+
+	public float wallSlideSpeedMax = 3;
+	public float wallStickTime = .25f;
+	float timeToWallUnstick;
+
+	float gravity;
+	float maxJumpVelocity;
+	float minJumpVelocity;
+	Vector3 velocity;
+	float velocityXSmoothing;
+
+	Controller2D controller;
+	MobileInput phone;
+
+	void Start() {
+		controller = GetComponent<Controller2D> ();
+		phone = GetComponent<MobileInput> ();
+
+		gravity = -(2 * maxJumpHeight) / Mathf.Pow (timeToJumpApex, 2);
+		maxJumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+		minJumpVelocity = Mathf.Sqrt (2 * Mathf.Abs (gravity) * minJumpHeight);
+		print ("Gravity: " + gravity + "  Jump Velocity: " + maxJumpVelocity);
+	}
+
+	void Update() {
+		bool touchSupported = true;
+		//Vector2 input = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+
+		Vector2 input = new Vector2(0, 0);
+		if (phone.GetGestures() == "taphold" || phone.GetGestures() == "right")
+			input = new Vector2(1, 1);
+		if (phone.GetGestures() == "left")
+			input = new Vector2(-1, 1);
+		print("In player: " +phone.GetGestures());
+
+
+		int wallDirX = (controller.collisions.left) ? -1 : 1;
+
+		float targetVelocityX = input.x * moveSpeed;
+		velocity.x = Mathf.SmoothDamp (velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below)?accelerationTimeGrounded:accelerationTimeAirborne);
+
+		bool wallSliding = false;
+		if ((controller.collisions.left || controller.collisions.right) && !controller.collisions.below && velocity.y < 0) {
+			wallSliding = true;
+
+			if (velocity.y < -wallSlideSpeedMax) {
+				velocity.y = -wallSlideSpeedMax;
+			}
+
+			if (timeToWallUnstick > 0) {
+				velocityXSmoothing = 0;
+				velocity.x = 0;
+
+				if (input.x != wallDirX && input.x != 0) {
+					timeToWallUnstick -= Time.deltaTime;
+				}
+				else {
+					timeToWallUnstick = wallStickTime;
+				}
+			}
+			else {
+				timeToWallUnstick = wallStickTime;
+			}
+
+		}
+
+		if (Input.GetKeyDown (KeyCode.Space) || phone.GetGestures() == "up" || phone.GetGestures() == "upleft" || phone.GetGestures() == "upright"	) {
+			if (phone.GetGestures() == "upleft")
+				//input = new Vector2 (-1, 1);
+			if (phone.GetGestures() == "upright")
+				//input = new Vector2 (1, 1);
+			
+			if (wallSliding) {
+				if (wallDirX == input.x) {
+					velocity.x = -wallDirX * wallJumpClimb.x;
+					velocity.y = wallJumpClimb.y;
+				}
+				else if (input.x == 0) {
+					velocity.x = -wallDirX * wallJumpOff.x;
+					velocity.y = wallJumpOff.y;
+				}
+				else {
+					velocity.x = -wallDirX * wallLeap.x;
+					velocity.y = wallLeap.y;
+				}
+			}
+			if (controller.collisions.below) {
+				velocity.y = maxJumpVelocity;
+			}
+		}
+		if (Input.GetKeyUp (KeyCode.Space) || phone.GetGestures() == "release") {
+			if (velocity.y > minJumpVelocity) {
+				velocity.y = minJumpVelocity;
+			}
+		}
+
+
+		velocity.y += gravity * Time.deltaTime;
+		controller.Move (velocity * Time.deltaTime, input);
+
+		if (controller.collisions.above || controller.collisions.below) {
+			velocity.y = 0;
+		}
+	}
+}
