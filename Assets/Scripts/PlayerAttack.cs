42313d756a6771873b7706cf1294c2fbac643e62
Rayne58 <mfalicea@buffalo.edu>
2016-04-04 11:38:27 -0400

    Got attack "working".  Now to make the different kinds!

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -7,13 +7,14 @@ public class PlayerAttack : MonoBehaviour
 	public LayerMask EnemyLayer;
 	public GameObject swordHilt;
 	public GameObject swordTip;
+	public GameObject owner;
+	public SpriteRenderer theSprite;
 
 	private bool attacking;
 
 	// Use this for initialization
 	void Start ()
 	{
-
 		attacking = false;
 		print ("SwordHilt at: " + swordHilt.transform.position + " SwordTip at: " + swordTip.transform.position);
 	}
@@ -21,29 +22,47 @@ public class PlayerAttack : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
+		
+		Vector3 temp = swordHilt.transform.position;
+		if (theSprite.flipX == false) {
+			temp.x = owner.transform.position.x + .84f;
+			temp.y = owner.transform.position.y - .43f;
+			swordHilt.transform.position = temp;
+
+			temp = swordTip.transform.position;
+			temp.x = owner.transform.position.x + 1.89f;
+			temp.y = owner.transform.position.y + .92f;
+			swordTip.transform.position = temp;
+		} else {
+			temp.x = owner.transform.position.x - .84f;
+			temp.y = owner.transform.position.y - .43f;
+			swordHilt.transform.position = temp;
+
+			temp = swordTip.transform.position;
+			temp.x = ((owner.transform.position.x - 1.89f));
+			temp.y = owner.transform.position.y + .92f;
+			swordTip.transform.position = temp;
+		}
+
 		if (attacking) {
 			Vector3 hitDir = swordTip.transform.position - swordHilt.transform.position;
-
 			float hitDistance = hitDir.magnitude;
 
-			//print ("Hit direction: " + hitDir + " hit Distance: " + hitDistance);
-			hitDir.Normalize();
-
-			RaycastHit2D hit = Physics2D.Raycast(swordHilt.transform.position, hitDir, hitDistance, EnemyLayer);
-			print (hit.ToString());
+			hitDir.Normalize ();
+			RaycastHit2D hit = Physics2D.Raycast (swordHilt.transform.position, hitDir, hitDistance, EnemyLayer);
 			Debug.DrawRay (swordHilt.transform.position, hitDir);
 			Debug.DrawRay (swordTip.transform.position, hitDir);
 			//print (hit.collider.name);
-			if (hit.collider != null)
-			{
-				print ("Hit ma niggaaa");
+			if (hit.collider != null) {
+				print ("Hit");
 				//Enemy e = hit.collider.gameObject.GetComponent<Enemy>();
 				//e.DamageEntity(Damage);
 			}
 		}
 	}
 
-	public void setAttack(bool attack) {
+	public void setAttack (bool attack)
+	{
 		attacking = attack;
 	}
 }
